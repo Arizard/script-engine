48535338083380422d4aab72e75cf878d42d0fed
Arie Oldman <arie.oldman@vhs7.tv>
2019-08-18 22:59:20 +1000

    Add drop shadow to the container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,19 +5,7 @@ import './Container.css'
 export default class Container extends Component {
     render() {
         return (
-            <div className='container'>
-<<<<<<< HEAD
-                {/* <div className='row'>
-                    <div className='col-12'>
-                        <Callout intent='warning' title='Prototype'>
-                            Please be careful! The service may be unreliable, so 
-                            please check the bottom left hand corner of the editor 
-                            to make sure your changes were saved!
-                        </Callout>
-                    </div>
-                </div> */}
-=======
->>>>>>> styling
+            <div className={'container '+Classes.ELEVATION_1}>
                 {this.props.children}
             </div>
         )
