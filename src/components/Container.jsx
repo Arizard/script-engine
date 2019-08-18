6653a5bba1381c04dc01766f0de962a88a0be72d
Arie Oldman <arie.oldman@vhs7.tv>
2019-08-18 22:55:55 +1000

    Remove callout

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,21 +5,7 @@ import './Container.css'
 export default class Container extends Component {
     render() {
         return (
-<<<<<<< Updated upstream
             <div className='container'>
-                <div className='row'>
-=======
-            <div className={'container '+Classes.ELEVATION_1}>
-                {/* <div className='row'>
->>>>>>> Stashed changes
-                    <div className='col-12'>
-                        <Callout intent='warning' title='Prototype'>
-                            Please be careful! The service may be unreliable, so 
-                            please check the bottom left hand corner of the editor 
-                            to make sure your changes were saved!
-                        </Callout>
-                    </div>
-                </div>
                 {this.props.children}
             </div>
         )
