f13d6884778076b05dd1043841dd93181053a64c
Arie Oldman <arie.oldman@vhs7.tv>
2019-08-19 14:12:07 +1000

    Remove tooltip from rowedit

--- src/components/RowEdit.jsx
+++ src/components/RowEdit.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
-import { Popover, Menu, Button, Divider, Tooltip } from '@blueprintjs/core'
+import { Popover, Menu, Button } from '@blueprintjs/core'
+import './RowEdit.css'
 
 export default class RowEdit extends Component {
     render() {
-        return (
+        return (<div className='row-edit'>
             <Popover content={
                 <Menu>
+                    <Menu.Divider title='Insert' />
                     <Menu.Item text='Insert header after' icon='header-one' onClick={this.props.onInsertHeaderAfter} />
                     <Menu.Item text='Insert script row after' icon='insert' onClick={this.props.onInsertScriptRowAfter} />
                     <Menu.Item text='Insert paragraph after' icon='paragraph' onClick={this.props.onInsertSimple} />
                     <Menu.Item text='Duplicate row' icon='duplicate' onClick={this.props.onDuplicateRow} />
+                    <Menu.Divider title='Arrange' />
                     <Menu.Item text='Move up' icon='chevron-up' onClick={this.props.onMoveUp} />
                     <Menu.Item text='Move down' icon='chevron-down' onClick={this.props.onMoveDown} />
-                    <Divider />
+                    <Menu.Divider title='Remove'/>
                     <Menu.Item text='Delete' intent='danger' icon='delete' onClick={this.props.onDeleteRow} />
                 </Menu>
             }>
-                <Tooltip content='Row Tools'>
-                    <Button icon='more' className='bp3-minimal row-edit' />
-                </Tooltip>
+                <Button icon='more' className='bp3-minimal' />
             </Popover>
-        )
+        </div>)
     }
 }
